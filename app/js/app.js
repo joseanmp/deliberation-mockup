a1665624db74954ec360dd018244a92ba754a4cf
Jose Antonio Moreno Punzon <josean@calvisitor-10-105-138-11.CalVisitor.1918.Berkeley.EDU>
2015-03-20 16:50:47 -0700

    maquetado

--- app/js/app.js
+++ app/js/app.js
@@ -2,25 +2,20 @@
 
 /* App Module */
 
-var phonecatApp = angular.module('phonecatApp', [
-  'ngRoute',
-  'phonecatControllers',
-  'phonecatFilters',
-  'phonecatServices'
+/*
+var deliberationApp = angular.module('deliberationApp', [
+  'ngRoute'
 ]);
 
-phonecatApp.config(['$routeProvider',
+deliberationApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
-      when('/phones', {
-        templateUrl: 'partials/phone-list.html',
-        controller: 'PhoneListCtrl'
-      }).
-      when('/phones/:phoneId', {
-        templateUrl: 'partials/phone-detail.html',
-        controller: 'PhoneDetailCtrl'
+      when('/deliberation', {
+        templateUrl: '../partials/deliberation.html'
       }).
       otherwise({
-        redirectTo: '/phones'
+        redirectTo: '/deliberation'
       });
   }]);
+
+*/
